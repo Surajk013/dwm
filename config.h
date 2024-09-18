0ac8957e0be785be6b3fae0b7d023f58086e9d5f
Surajk013 <surajsinghk013@gmail.com>
2024-09-18 15:19:28 +0530

    2024-09-18

--- config.h
+++ config.h
@@ -23,7 +23,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "📝", "🌐", "< >","</ >", "🎮" };
+static const char *tags[] = { "","🌐","🖥️","< >","</ >","🎮" };
 
 static const Rule rules[] = {
 	/* xprop(1):
